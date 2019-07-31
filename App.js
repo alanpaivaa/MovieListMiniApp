af85886c4d02984edae4f8289004e8e7f7e3ad32
Alan Paiva <alan.jeferson11@gmail.com>
2019-07-31 18:00:52 -0300

    Add basic list of movies with mocked data

--- App.js
+++ App.js
@@ -7,14 +7,63 @@
  */
 
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList, Image, StyleSheet } from 'react-native';
 
-export default class App extends Component {
+const styles = StyleSheet.create({
+  movieCell: {
+    paddingTop: 10
+  },
+  movieTitle: {
+    fontSize: 17,
+    fontWeight: "bold"
+  },
+  movieInfo: {
+    fontSize: 12,
+    color: "gray"
+  }
+});
+
+class Cell extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello MovieListMiniApp!</Text>
-      </View>
+        <View style={styles.movieCell}>
+          <Image source={this.props.movie.image} style={{width: 50, height: 50}}/>
+          <Text style={styles.movieTitle}>{this.props.movie.title}</Text>
+          <Text style={styles.movieInfo}>{this.props.movie.year}</Text>
+        </View>
+    )
+  }
+}
+
+export default class App extends Component {
+  state = {
+    movies: [
+      {
+        id: 1,
+        title: "The Lion King",
+        year: 2019,
+        image: {
+          uri: "https://pbs.twimg.com/profile_images/1134173490620354561/V9Au2vRZ_400x400.jpg"
+        }
+      }, {
+        id: 2,
+        title: "The Avengers - Endgame",
+        year: 2019,
+        image: {
+          uri: "https://images-na.ssl-images-amazon.com/images/I/91epzdXTTHL._SX300_.jpg"
+        }
+      }
+    ]
+  };
+
+  render() {
+    return (
+        <View style={{flex: 1}}>
+          <FlatList
+              data={this.state.movies}
+              renderItem={({item}) => <Cell movie={item} />}
+          />
+        </View>
     );
   }
 }
